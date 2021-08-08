02f2813e43ca1b8fbdc78573ba78ece01220fc72
ralf-boltshauser <ralf@boltshauser.com>
2021-08-08 17:38:31 +0200

    Orientation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'qwixx';
 
   constructor() {
-    
+    screen.orientation.lock('landscape').then(() => {
+      console.log('Orientation locked!');
+    });
   }
-
-
 }
